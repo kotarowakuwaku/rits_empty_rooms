abd5114027d532ddc5541239dd05f3a38178e607
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-03-14 22:30:02 +0900

    firebase準備

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { initializeFirebaseApp } from '@/lib/firebase/firebase'
 
+
+initializeFirebaseApp()
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
