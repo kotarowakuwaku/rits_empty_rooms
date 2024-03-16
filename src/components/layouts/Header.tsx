97003c9ca735ce30379cd47aab1d3f1992227527
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-03-16 16:36:55 +0900

    Header作成

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1, zIndex:"800"}}>
+      <AppBar sx={{backgroundColor:"#990000"}}>
+        <Toolbar variant="dense">
+            <Typography variant="h6" color="inherit" component="div">
+            RitsEmptyRooms
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <IconButton edge="start" color="inherit" aria-label="menu" >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
