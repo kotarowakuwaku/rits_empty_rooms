8823accc67c86634c6711ea498f1cfd1f33c538f
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-03-17 11:43:07 +0900

    TabButtonを値を取得できるように修正 キャンパスを区別するための型を作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,35 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import db from "@/lib/firebase/firebase";
-import { useEffect, useState } from "react";
+import { SyntheticEvent, useEffect, useState } from "react";
 import {collection, getDocs} from 'firebase/firestore';
 import  Header  from "@/components/layouts/Header";
 import TabButton from "@/components/elements/TabButton"
 import { Box } from "@mui/material";
-import SelectTimeTable from "@/components/elements/SelectTimeTable";
+import SelectTimeTable from "@/components/elements/ToggleButton";
+import { CAMPUS_MODE, type CampusMode } from "@/types/CampusMode";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [rooms, setRooms] = useState([]);
+  // const [rooms, setRooms] = useState([]);
 
-  useEffect(()=>{
-    const roomData = collection(db,"rooms");
-    getDocs(roomData).then((snapShot)=>{
+  // useEffect(()=>{
+  //   const roomData = collection(db,"rooms");
+  //   getDocs(roomData).then((snapShot)=>{
       // console.log(snapShot.docs.map((doc)=>({...doc.data()})));
       // setRooms(snapShot.docs.map((doc) => ({...doc.data() })));
-    });
-  },[]);
+  //   });
+  // },[]);
+  const [mode, setMode] = useState<CampusMode>(CAMPUS_MODE.LeftName);
+  // const tabIndex = mode === CAMPUS_MODE.LeftName ? 0 : mode === CAMPUS_MODE.CenterName ? 1 : 2;
+
+  const handleSwitch = (event: SyntheticEvent<Element, Event>, newAlignment: CampusMode | null) => {
+    if (newAlignment !== null) {
+      setMode(newAlignment);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -27,10 +37,8 @@ export default function Home() {
       </Head>
       <main>
       <Header />
-      <Box sx={{marginTop: "48px"}}>
-
-      </Box>
-      <TabButton />
+      <Box sx={{marginTop: "48px"}} />
+      <TabButton leftName={CAMPUS_MODE.LeftName} centerName={CAMPUS_MODE.CenterName} rightName={CAMPUS_MODE.RightName} value={mode} onChange={handleSwitch}/>
       <SelectTimeTable />
       {/* <div>
         {rooms.map((room) => (
@@ -40,6 +48,9 @@ export default function Home() {
           </div>
         ))}
       </div> */}
+      <div>
+        {mode}キャンパスのページです
+      </div>
       </main>
     </>
   );
