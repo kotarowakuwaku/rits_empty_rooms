f41405e0ffbe359575c0c8fa11e1db46c8a3332f
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-03-17 22:11:07 +0900

    format調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,12 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import db from "@/lib/firebase/firebase";
 import { SyntheticEvent, useEffect, useState } from "react";
-import { collection, getDocs, QuerySnapshot, DocumentData } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  QuerySnapshot,
+  DocumentData,
+} from "firebase/firestore";
 import Header from "@/components/layouts/Header";
 import TabButton from "@/components/elements/TabButton";
 import { Box } from "@mui/material";
@@ -38,7 +43,7 @@ export default function Home() {
   ) => {
     if (newAlignment !== null) {
       setCampus(newAlignment);
-      setRefreshKey(old => old + 1);
+      setRefreshKey((old) => old + 1);
     }
   };
 
@@ -51,7 +56,7 @@ export default function Home() {
   ) => {
     if (newDay !== null) {
       setDay(newDay);
-      setRefreshKey(old => old + 1);
+      setRefreshKey((old) => old + 1);
     }
   };
 
@@ -61,7 +66,7 @@ export default function Home() {
   ) => {
     if (newTime !== null) {
       setTime(newTime);
-      setRefreshKey(old => old + 1);
+      setRefreshKey((old) => old + 1);
     }
   };
 
@@ -106,33 +111,36 @@ export default function Home() {
           {rooms.map((emptyRooms) => {
             return (
               <>
-              <h2>クリエーションコア</h2>
-              <div key={emptyRooms.rooms}>
-                {emptyRooms.rooms.map((emptyRoom:string) => {
-                  if (emptyRoom.indexOf("cc") !== -1) {
-                    return (
-                      <div key={emptyRoom}>
-                        <p>{emptyRoom}</p>
-                      </div>
-                    );
-                  }
-                  return null; // 条件に一致しない場合は null を返す
-                })}
-              </div>
-              <h2>コラーニングⅠ</h2>
-              <div key={emptyRooms.rooms}>
-              {emptyRooms.rooms.map((emptyRoom:string) => {
-                if (emptyRoom.match(/^c\d/gi) !== null ) {
-                  return (
-                    <div key={emptyRoom} style={{float:'left',margin:'0 5px'}}>
-                      <div>{emptyRoom}</div>
-                    </div>
-                  );
-                }
-                return null; // 条件に一致しない場合は null を返す
-              })}
-            </div>
-            </>
+                <h2>クリエーションコア</h2>
+                <div key={emptyRooms.rooms}>
+                  {emptyRooms.rooms.map((emptyRoom: string) => {
+                    if (emptyRoom.indexOf("cc") !== -1) {
+                      return (
+                        <div key={emptyRoom}>
+                          <p>{emptyRoom}</p>
+                        </div>
+                      );
+                    }
+                    return null; // 条件に一致しない場合は null を返す
+                  })}
+                </div>
+                <h2>コラーニングⅠ</h2>
+                <div key={emptyRooms.rooms}>
+                  {emptyRooms.rooms.map((emptyRoom: string) => {
+                    if (emptyRoom.match(/^c\d/gi) !== null) {
+                      return (
+                        <div
+                          key={emptyRoom}
+                          style={{ float: "left", margin: "0 5px" }}
+                        >
+                          <div>{emptyRoom}</div>
+                        </div>
+                      );
+                    }
+                    return null; // 条件に一致しない場合は null を返す
+                  })}
+                </div>
+              </>
             );
           })}
         </div>
