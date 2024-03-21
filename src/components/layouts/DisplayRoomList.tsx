ff51b6def2ac731e5803660f9c2bf24520928dff
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-03-21 18:08:39 +0900

    空き教室の表示をコンポーネント化

---
+++ src/components/layouts/DisplayRoomList.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface DisplayRoomListProps {
+  BuildingName: string[];
+  BuildingNameObject: {[key: string]: boolean};
+}
+
+export default function DisplayRoomList({
+    BuildingName = [],
+    BuildingNameObject = {}
+}: DisplayRoomListProps) {
+    return (
+
+    BuildingName.map((CheckEmptyRoom: string) => {
+        const roomNumbers = CheckEmptyRoom.match(/\d+/g)?.[0] || "";
+        return (
+          <>
+            {parseFloat(roomNumbers) % 10 === 1 &&
+            Math.floor(parseFloat(roomNumbers) / 100) !== 1 ? (
+              <br />
+            ) : null}
+            <div
+              key={CheckEmptyRoom}
+              style={{
+                float: "left",
+                margin: "0 5px",
+                opacity: BuildingNameObject[CheckEmptyRoom] ? "1" : "0.1",
+              }}
+            >
+              <div>{CheckEmptyRoom}</div>
+            </div>
+          </>
+        );
+      })
+    );
+}
