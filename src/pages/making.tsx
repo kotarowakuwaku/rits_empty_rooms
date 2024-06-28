6249ad2b2bdc5e1e8d9da4e02faff2ab3df0da05
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-06-28 19:41:22 +0900

    作成中の画面作成

--- src/pages/making.tsx
+++ src/pages/making.tsx
@@ -1,12 +1,57 @@
 import Header from "@/components/layouts/Header";
+import { Box } from "@mui/material";
+import Image from "next/image";
+import Head from "next/head";
 
 export default function Document() {
   return (
     <>
-      <head></head>
-      <main>
+      <Head>
+        <title>RitsEmptyRooms</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+        <link rel="icon" href="favicon.ico" />
+        <meta name="theme-color" content="#EDEAE8" />
+      </Head>
+      <main
+        style={{
+          backgroundColor: "#EDEAE8",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Header />
-        <h1>製作中のページです</h1>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+            width: "100%",
+          }}
+        >
+          <h3>作成中の画面です</h3>
+        </Box>
+        <Box
+          sx={{
+            position: "fixed",
+            bottom: 0,
+            right: 30,
+            width: "50%",
+            maxWidth: "196px",
+          }}
+        >
+          <Image
+            src="/RER_logo.png"
+            width={256}
+            height={256}
+            layout="responsive"
+            alt=""
+          />
+        </Box>
       </main>
     </>
   );
