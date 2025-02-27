17550aec4e8d1dc7317284c1abed213fdb8e46bd
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2025-02-27 17:12:04 +0900

    roomsをbuilding_idでフィルタリングして返す

--- src/pages/fetchDataTect.tsx
+++ src/pages/fetchDataTect.tsx
@@ -2,7 +2,16 @@ import React, { useEffect, useState } from 'react'
 import { supabase } from '@/lib/supabase/supabase'
 
 const fetchDataTect = () => {
-    const [data, setData] = useState<any[]>([])
+    const [data, setData] = useState<Record<string, any[]>>({})
+
+    const groupByKey = <T, K extends keyof T>(array: T[], key: K): Record<string, T[]> =>
+        array.reduce((acc, item) => {
+          const groupKey = String(item[key]);
+          acc[groupKey] = acc[groupKey] ?? [];
+          acc[groupKey].push(item);
+          return acc;
+        }, {} as Record<string, T[]>);
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -14,11 +23,11 @@ const fetchDataTect = () => {
                 if (roomsError) {
                     throw roomsError
                 }
-                setData(roomsData || null);
-                console.log(roomsData);
+                console.log(groupByKey(roomsData, 'building_id'))
+                setData(groupByKey(roomsData, 'building_id') || {})
             } catch (error) {
                 console.log('error', error)
-                setData([])
+                setData({})
             }
         }
 
@@ -29,7 +38,19 @@ const fetchDataTect = () => {
 
     return (
         <>
-
+            <h1>Fetch Data Test</h1>
+            <ul>
+                {Object.keys(data).map((key) => (
+                    <li key={key}>
+                        <h2>{key}</h2>
+                        <ul>
+                            {data[key].map((room) => (
+                                <li key={room.id}>{room.name}</li>
+                            ))}
+                        </ul>
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
