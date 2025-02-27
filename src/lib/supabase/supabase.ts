da44996bc9f430f8d60e543e634cbf810a0b8f40
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2025-02-27 15:20:44 +0900

    supabaseセットアップ

---
+++ src/lib/supabase/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+// Create a single supabase client for interacting with your database
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
