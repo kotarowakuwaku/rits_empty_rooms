51f16ac2d31a5de97972e3bb03db7474929e98e8
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2025-02-28 13:21:29 +0900

    format調整

--- src/lib/supabase/supabase.ts
+++ src/lib/supabase/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 // Create a single supabase client for interacting with your database
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
--- src/pages/fetchDataTect.tsx
+++ src/pages/fetchDataTect.tsx
@@ -1,58 +1,61 @@
-import React, { useEffect, useState } from 'react'
-import { supabase } from '@/lib/supabase/supabase'
+import React, { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase/supabase";
 
 const fetchDataTect = () => {
-    const [data, setData] = useState<Record<string, any[]>>({})
+  const [data, setData] = useState<Record<string, any[]>>({});
 
-    const groupByKey = <T, K extends keyof T>(array: T[], key: K): Record<string, T[]> =>
-        array.reduce((acc, item) => {
-          const groupKey = String(item[key]);
-          acc[groupKey] = acc[groupKey] ?? [];
-          acc[groupKey].push(item);
-          return acc;
-        }, {} as Record<string, T[]>);
+  const groupByKey = <T, K extends keyof T>(
+    array: T[],
+    key: K,
+  ): Record<string, T[]> =>
+    array.reduce(
+      (acc, item) => {
+        const groupKey = String(item[key]);
+        acc[groupKey] = acc[groupKey] ?? [];
+        acc[groupKey].push(item);
+        return acc;
+      },
+      {} as Record<string, T[]>,
+    );
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data: roomsData, error: roomsError } = await supabase
+          .from("rooms")
+          .select("*");
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const { data: roomsData, error: roomsError } = await supabase
-                    .from('rooms')
-                    .select('*')
-
-                if (roomsError) {
-                    throw roomsError
-                }
-                console.log(groupByKey(roomsData, 'building_id'))
-                setData(groupByKey(roomsData, 'building_id') || {})
-            } catch (error) {
-                console.log('error', error)
-                setData({})
-            }
+        if (roomsError) {
+          throw roomsError;
         }
+        console.log(groupByKey(roomsData, "building_id"));
+        setData(groupByKey(roomsData, "building_id") || {});
+      } catch (error) {
+        console.log("error", error);
+        setData({});
+      }
+    };
 
-        fetchData();
-    }, [])
+    fetchData();
+  }, []);
 
-
-
-    return (
-        <>
-            <h1>Fetch Data Test</h1>
+  return (
+    <>
+      <h1>Fetch Data Test</h1>
+      <ul>
+        {Object.keys(data).map((key) => (
+          <li key={key}>
+            <h2>{key}</h2>
             <ul>
-                {Object.keys(data).map((key) => (
-                    <li key={key}>
-                        <h2>{key}</h2>
-                        <ul>
-                            {data[key].map((room) => (
-                                <li key={room.id}>{room.name}</li>
-                            ))}
-                        </ul>
-                    </li>
-                ))}
+              {data[key].map((room) => (
+                <li key={room.id}>{room.name}</li>
+              ))}
             </ul>
-        </>
-    )
-}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-export default fetchDataTect
+export default fetchDataTect;
